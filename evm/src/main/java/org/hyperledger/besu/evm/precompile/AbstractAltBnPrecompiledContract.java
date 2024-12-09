802d1ec80b50dd1fda78bf6e8bf1776d132f4df0
garyschulte <garyschulte@gmail.com>
2024-12-09 10:37:29 -0800

    implement split g1/g2 discount tables from https://github.com/ethereum/EIPs/pull/9116/files
    Signed-off-by: garyschulte <garyschulte@gmail.com>

--- evm/src/main/java/org/hyperledger/besu/evm/precompile/AbstractAltBnPrecompiledContract.java
+++ evm/src/main/java/org/hyperledger/besu/evm/precompile/AbstractAltBnPrecompiledContract.java
@@ -26,7 +26,6 @@ import javax.annotation.Nonnull;
 
 import com.sun.jna.ptr.IntByReference;
 import org.apache.tuweni.bytes.Bytes;
-import org.hyperledger.besu.nativelib.gnark.LibGnarkEIP2537;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
